82cfd151200a418647681dc9f583af23741368f7
Dhanraj Khodaliya <dhanraj.khodaliya@xylyx.com>
2018-10-01 18:41:15 +0530

    01-10-2018

--- src/main/java/com/test/StompTest.java
+++ src/main/java/com/test/StompTest.java
@@ -11,16 +11,13 @@ import ua.naiksoftware.stomp.client.StompClient;
 public class StompTest {
 
 	public static void main(String[] args) {
-		final StompClient mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://lite.bcommo.in:3030/liteapi/mlive-channel");
+		//lite.bcommo.in
+		final StompClient mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://localhost:3030/liteapi/mlive-channel");
 		mStompClient.connect();
-
-		mStompClient.send("/app/topic/watch/5b8a2e7df3391b75cb9441dd",ApplicationUtils.generateJSONFromObject(new ClientRequest())).subscribe();
-		try {
-			Thread.sleep(1000);
-		} catch (final InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		final ClientRequest onopenClientRequest =new ClientRequest();
+		onopenClientRequest.setIsMobile(Boolean.TRUE);
+		onopenClientRequest.setScripts(Arrays.asList("NSE-ADANIENT-27/09/2018","NSE-AJANTPHARM-27/09/2018"));
+		mStompClient.send("/app/topic/watch/5b8a2e7df3391b75cb9441dd",ApplicationUtils.generateJSONFromObject(onopenClientRequest)).subscribe();
 		//mStompClient.send("/app/topic/login", "zBnir85bs66xMTdZxzTKaXOwgf9+1HpIuc+AMCEVz40KW9/7k7+a4/vA60VEXQEaRh+p2S/FlYbgcwD2D+0kQQ==").subscribe();
 
 		mStompClient.topic("/topic/watch/5b8a2e7df3391b75cb9441dd").subscribe(topicMessage -> {
@@ -33,46 +30,46 @@ public class StompTest {
 		clientRequest.setScripts(Arrays.asList("NSE-ADANIENT-27/09/2018","NSE-AJANTPHARM-27/09/2018"));
 		clientRequest.setWantToStart(Boolean.FALSE);
 		clientRequest.setWantToStop(Boolean.FALSE);
-		try {
+		/*try {
 			Thread.sleep(1000);
 		} catch (final InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		mStompClient.send("/app/topic/watch/5b8a2e7df3391b75cb9441dd",ApplicationUtils.generateJSONFromObject(clientRequest)).subscribe();
-		try {
+		}*/
+		//mStompClient.send("/app/topic/watch/5b8a2e7df3391b75cb9441dd",ApplicationUtils.generateJSONFromObject(clientRequest)).subscribe();
+		/*try {
 			Thread.sleep(2000);
 		} catch (final InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 
-		final ClientRequest clientRequest2=new ClientRequest();
+		/*final ClientRequest clientRequest2=new ClientRequest();
 		clientRequest2.setIsMobile(Boolean.TRUE);
 		clientRequest2.setIsFirstTime(Boolean.FALSE);
 		clientRequest2.setWantToStop(false);
 		clientRequest2.setWantToStart(true);
 		mStompClient.send("/app/topic/watch/5b8a2e7df3391b75cb9441dd",ApplicationUtils.generateJSONFromObject(clientRequest2)).subscribe();
-		mStompClient.lifecycle().subscribe(lifecycleEvent -> {
-			switch (lifecycleEvent.getType()) {
+		 */mStompClient.lifecycle().subscribe(lifecycleEvent -> {
+			 switch (lifecycleEvent.getType()) {
 
-			case OPENED:
-				System.out.println( "Stomp connection opened");
-				break;
+			 case OPENED:
+				 System.out.println( "Stomp connection opened");
+				 break;
 
-			case ERROR:
-				System.out.println(  "Error" + lifecycleEvent.getException());
-				break;
+			 case ERROR:
+				 System.out.println(  "Error" + lifecycleEvent.getException());
+				 break;
 
-			case CLOSED:
-				System.out.println( "Stomp connection closed");
-				break;
-			}
-		});
+			 case CLOSED:
+				 System.out.println( "Stomp connection closed");
+				 break;
+			 }
+		 });
 
-		// ...
+		 // ...
 
-		//mStompClient.disconnect();
+		 //mStompClient.disconnect();
 
 	}
 
